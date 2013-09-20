cd211a9a94b915b85001faf97df723b95161edb9
Ryan Coppolo <r@coppolo.com>
2013-09-20 03:28:12 +0000

    Resource get & put.

--- lib/rex/resource.ex
+++ lib/rex/resource.ex
@@ -4,12 +4,20 @@ defmodule Rex.Resource do
 
     quote do
 
-      def ping do
-        Rex.Server.ping
+      def put(key, value) do
+        Rex.Server.put(bucket, key, value)
+      end
+
+      def get(key) do
+        Rex.Server.get(bucket, key)
       end
 
       def bucket do
         atom_to_binary(__MODULE__) |> String.split(".") |> List.last
+      end
+
+      def ping do
+        Rex.Server.ping
       end
 
       defoverridable [bucket: 0]
--- lib/rex/server.ex
+++ lib/rex/server.ex
@@ -10,15 +10,35 @@ defmodule Rex.Server do
     :gen_server.call(__MODULE__, :ping)
   end
 
-  def init([]) do
-    {:ok, config} = :application.get_env(:rex, :dev)
-    {:ok, riak} = :riakc_pb_socket.start_link(config[:host], config[:port])
-    {:ok, riak}
+  def put(bucket, key, value) do
+    :gen_server.call(__MODULE__, {:put, bucket, key, value})
   end
 
-  def handle_call(:ping, _from, riak) do
-    resp = :riakc_pb_socket.ping(riak)
-    {:reply, resp, riak}
+  def get(bucket, key) do
+    :gen_server.call(__MODULE__, {:get, bucket, key})
+  end
+
+  def init([]) do
+    {:ok, config} = :application.get_env(:rex, :dev)
+    {:ok, pid} = :riakc_pb_socket.start_link(config[:host], config[:port])
+    {:ok, pid}
+  end
+
+  def handle_call(:ping, _from, pid) do
+    resp = :riakc_pb_socket.ping(pid)
+    {:reply, resp, pid}
+  end
+
+  def handle_call({:put, bucket, key, value}, _from, pid) do
+    obj = :riakc_obj.new(bucket, key, value)
+    resp = :riakc_pb_socket.put(pid, obj)
+    {:reply, resp, pid}
+  end
+
+  def handle_call({:get, bucket, key}, _from, pid) do
+    {:ok, obj} = :riakc_pb_socket.get(pid, bucket, key)
+    resp = :riakc_obj.get_value(obj)
+    {:reply, resp, pid}
   end
 
 end
